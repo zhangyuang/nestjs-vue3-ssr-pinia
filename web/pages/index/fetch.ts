882b18d2a2f6052dfb82ced5be0f197cc58b8b8a
yuuangzhang <yuuangzhang@tencent.com>
2022-07-15 23:03:33 +0800

    chore: update example

--- web/pages/index/fetch.ts
+++ web/pages/index/fetch.ts
@@ -14,7 +14,7 @@ interface Params {
   pinia: Pinia
 }
 
-export default async ({ store, router,pinia }: Params, ctx?: ISSRNestContext<{
+export default async ({ store, router, pinia }: Params, ctx?: ISSRNestContext<{
   apiService?: IApiService
 }>) => {
   // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
