9b3b7916253b05fdec04bfceebd888e9f0907d12
yuuangzhang <yuuangzhang@tencent.com>
2022-07-15 23:03:02 +0800

    chore: update example

--- web/pages/detail/fetch.ts
+++ web/pages/detail/fetch.ts
@@ -1,5 +1,6 @@
 import { Store } from 'vuex'
 import { RouteLocationNormalizedLoaded } from 'vue-router'
+import { Pinia } from 'pinia'
 import { ISSRNestContext } from 'ssr-types'
 import { Ddata } from '~/typings/data'
 import { useDetailStore } from '@/pinia-store'
@@ -8,12 +9,13 @@ interface ApiDeatilservice {
 }
 interface Params {
   store: Store<any>
-  router: RouteLocationNormalizedLoaded
+  router: RouteLocationNormalizedLoaded,
+  pinia: Pinia
 }
-export default async ({ store, router }: Params, ctx?: ISSRNestContext<{
+export default async ({ store, router, pinia }: Params, ctx?: ISSRNestContext<{
   apiDeatilservice?: ApiDeatilservice
 }>) => {
-  const detailStore = useDetailStore()
+  const detailStore = useDetailStore(pinia)
   // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
   const data = __isBrowser__ ? await (await window.fetch(`/api/detail/${router.params.id}`)).json() : await ctx?.apiDeatilservice?.index(ctx.request.params.id)
   detailStore.setData(data)
--- web/pages/index/fetch.ts
+++ web/pages/index/fetch.ts
@@ -1,6 +1,7 @@
 import { Store } from 'vuex'
 import { RouteLocationNormalizedLoaded } from 'vue-router'
 import { ISSRNestContext } from 'ssr-types'
+import { Pinia } from 'pinia'
 import { IndexData } from '~/typings/data'
 import { useIndexStore } from '@/pinia-store'
 
@@ -9,14 +10,15 @@ interface IApiService {
 }
 interface Params {
   store: Store<any>
-  router: RouteLocationNormalizedLoaded
+  router: RouteLocationNormalizedLoaded,
+  pinia: Pinia
 }
 
-export default async ({ store, router }: Params, ctx?: ISSRNestContext<{
+export default async ({ store, router,pinia }: Params, ctx?: ISSRNestContext<{
   apiService?: IApiService
 }>) => {
   // 阅读文档获得更多信息 http://doc.ssr-fc.com/docs/features$fetch#%E5%88%A4%E6%96%AD%E5%BD%93%E5%89%8D%E7%8E%AF%E5%A2%83
-  const indexStore = useIndexStore()
+  const indexStore = useIndexStore(pinia)
   const data = __isBrowser__ ? await (await window.fetch('/api/index')).json() : await ctx?.apiService?.index()
   indexStore.setData(data)
 }
